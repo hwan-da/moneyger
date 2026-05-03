2f32edad38437fb5cee979bc3954b7d5b24bdb2c
hwan-da <sinphoto@naver.com>
2026-05-03 18:45:52 +0900

    for deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
 import { handlers } from "@/lib/auth"
 
-export const { GET, POST } = handlers
+export const { GET, POST } = export async function GET() {
+    return Response.json({ ok: true });
+}handlers
